abce06ced75e9f9d8cd56ec357f87d0161a4f157
MMasterZ <soulhunz@hotmail.com>
2020-08-20 17:31:17 +0700

    Auto stash before merge of "master" and "origin/master"

--- upload_data.php
+++ upload_data.php
@@ -7,36 +7,16 @@ $fname = $_FILES['file']['tmp_name'];
 
 $real_name = $_FILES['file']['name'];
 
-
 $uploads_dir = "./uploads";
-
-
 
 move_uploaded_file($fname, "$uploads_dir/$real_name");
 
-
-
 $real_file_destination = $uploads_dir . "/" . $real_name;
 
-
 echo $real_file_destination;
-
-// if(file_exists($real_file_destination)){
-//     echo "EXISTS";
-
-// }else{
-//     echo "NOT EXISTS";
-
-// }
-
-
-
 
 $sql = "load data local infile '" . $real_file_destination . "' into table country_list fields terminated by ','  optionally enclosed by '\"' ignore 1 lines (col1,col2,col3,col4,col5,col6,col7)";
 
 mysqli_query($sqli,$sql) or die(mysqli_error($sqli));
-
-
-
 
 ?>
