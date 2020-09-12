ddf3797c7ad6a9eb9d866e3449b2561fed740455
art <pakkawat.kh@gmail.com>
2020-09-12 14:22:37 +0700

    update

--- indicator_imp_cont2.php
+++ indicator_imp_cont2.php
@@ -1,10 +1,9 @@
-<?
-//1. Gross exports used in importer's consumption (Imp_cons)
+<?php 
+//5. Imported content in gross exports (Imp_cont)
 //imp_country = import country ส่งมาเป็นรหัสประเทศ 3 ตัว
 //exp_country = export country ส่งมาเป็นรหัสประเทศ 3 ตัว
 //year = ปี ส่งเป็น ค.ศ. 2017
 //sector = sector ส่งมาเป็น id ตัวเลข sector 
-
 require_once('connection.php');
 require_once('sector_data.php');
 
@@ -18,6 +17,8 @@ $sector = $data['sector'];
 // $imp_country = ['CHN','THA','JPN'];
 // $year = ['2007','2017'];
 // $sector = [1,3,5];
+
+$tableName = $exp_country . "_" . $year;
 
 // Import Country
 $impText = ' and imp_country in (';
@@ -37,12 +38,13 @@ if($sector[0] != 0 ){
 $sectorText = substr($sectorText,0,-1);
 $sectorText = $sectorText . ")";
 }
+
 $final = [];
 
 foreach($exp_country as $expData){
     foreach($year as $yearData){
         $tableName =  $expData . "_" . $yearData;
-        $sql  = "select sum(value) as sum,exp_country, imp_country,exp_sector, year  from " . $tableName . " where (variable = 'DVA_FIN' or variable='DVA_INT') ". $impText . $sectorText . " group by imp_country, exp_sector" ;
+        $sql  = "select sum(value) as sum,exp_country, imp_country,exp_sector, year  from " . $tableName . " where (variable = 'MVA_FIN' or variable='MVA_INT'  or variable='OVA_FIN' or variable='OVA_INT') ". $impText . $sectorText . " group by imp_country, exp_sector" ;
         $value = $db->query($sql)->fetchAll();
         $final = array_merge($final,$value);
         
@@ -58,7 +60,7 @@ for($i=0;$i< count($final);$i++){
     $result[$i]['variable_set'] = "-";
     $result[$i]['value'] = round($final[$i][0],2);
     $result[$i]['year'] = $final[$i][4];
-    $result[$i]['indicator'] = 'imp_cons';
+    $result[$i]['indicator'] = 'Imp_cont';
 }
 echo json_encode($result);
 ?>
